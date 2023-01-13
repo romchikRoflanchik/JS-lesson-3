e9b5284fcd784c83419aab794dd790bdd6b13f64
romchikRoflanchik <romabill@ukr.net>
2023-01-13 06:20:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//  Завдання 1
+
+let n1 = prompt();
+let result = 1;
+for(let  i=1; i<=n1; i++){
+    result *= i;
+    console.log(result);
+}
+
+// Завдання 2
+
+
+// for(i=1000; i<=9999; i+=3){
+//     document.write(`<h2> ${i}</h2>`)
+// }
+
+// Завдання 3
+
+// for(i=1; i<=55 ; i+=2){
+//     document.write(`<h2> ${i}</h2>`)
+// }
+
+// Завдання 4
+
+// for(i=90; i>=0; i-=5){
+//     document.write(`<h2> ${i}</h2>`)
+// }
+
+// Завдання 5
+
+// let result = 1;
+// for(i=1;i<=20;i++){
+//     document.write(`<h2> ${result*=2}</h2>`)
+// }
+
+// Завдання 6
+
+// let result = 1
+// for(i=2;i<10000;i=2*i-1){
+//     document.write(`<h2> ${i}</h2>`)
+// }
+
+// Завдання 7
+
+// for(i=-166;i<100;i=2*i+200){
+//     if(i>-100){
+//      document.write(`<h2> ${i}</h2>`)
+//     }
+//  }
+
+// Завдання 8
+
+// let a = +prompt('Введіть число'); 
+// let b = +prompt('Введіть степінь');
+// let result = 1;
+// for(let i=1; i<=b || i<=-b; i++){
+//     result *= a ;
+// }
+// if(b<0){
+//    result=1/result 
+// }
+// else{
+//    result=1
+// }
+// console.log(result);
